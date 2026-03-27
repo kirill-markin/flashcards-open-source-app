b4c5053638dac0b3906953b29335199c29a87bfa
Kirill Markin <markinkirill@gmail.com>
2026-03-27 16:56:53 +0100

    Align Android splash and launcher branding

--- apps/android/app/src/main/java/com/flashcardsopensourceapp/app/MainActivity.kt
+++ apps/android/app/src/main/java/com/flashcardsopensourceapp/app/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
